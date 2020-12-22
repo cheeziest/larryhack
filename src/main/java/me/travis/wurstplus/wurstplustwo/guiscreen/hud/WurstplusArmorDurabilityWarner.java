9d46009936acfd29f06dd3c9a7388aca7759fb8f
cheeziest <71726309+cheeziest@users.noreply.github.com>
2020-12-22 15:10:45 -0600

    Update WurstplusArmorDurabilityWarner.java

--- src/main/java/me/travis/wurstplus/wurstplustwo/guiscreen/hud/WurstplusArmorDurabilityWarner.java
+++ src/main/java/me/travis/wurstplus/wurstplustwo/guiscreen/hud/WurstplusArmorDurabilityWarner.java
@@ -15,7 +15,7 @@ public class WurstplusArmorDurabilityWarner extends WurstplusPinnable { // STILL
     @Override
 	public void render() {
 
-		String line = "ur armor is kinda low rn :/";
+		String line = "btw ur armor is kinda low man";
 
         if (is_damaged()) {
             create_line(line, this.docking(1, line), 2, 255, 20, 20, 255);
